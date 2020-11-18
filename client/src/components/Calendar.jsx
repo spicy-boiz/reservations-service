aa70a190cb0418887292901e96ca5ec2d0977fce
Chris <chrisldiep@gmail.com>
2020-11-17 17:46:56 -0800

    Completed scrolling calendar bar with no css

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -3,16 +3,23 @@ import styles from './Calendar.css';
 
 function Calendar() {
   const [dateLeft, setDateLeft] = useState(new Date());
-  let oneMonthUp = (new Date(dateLeft)).setMonth(dateLeft.getMonth() + 1);
+  const oneMonthUp = (new Date(dateLeft)).setMonth(dateLeft.getMonth() + 1);
   const [dateRight, setDateRight] = useState(new Date(oneMonthUp));
-
+  function decreaseMonth() {
+    setDateLeft(new Date(dateLeft.setMonth(dateLeft.getMonth() - 1)));
+    setDateRight(new Date(dateRight.setMonth(dateRight.getMonth() - 1)));
+  }
+  function increaseMonth() {
+    setDateLeft(new Date(dateLeft.setMonth(dateLeft.getMonth() + 1)));
+    setDateRight(new Date(dateRight.setMonth(dateRight.getMonth() + 1)));
+  }
   return (
     <div id={styles.calendarContainer}>
       <div id={styles.calendarBar}>
-        <span>{'<'}</span>
+        <button type="button" onClick={decreaseMonth}>{'<'}</button>
         <span>{`${dateLeft.toLocaleString('default', { month: 'long', year: 'numeric' })}`}</span>
         <span>{`${dateRight.toLocaleString('default', { month: 'long', year: 'numeric' })}`}</span>
-        <span>{'>'}</span>
+        <button type="button" onClick={increaseMonth}>{'>'}</button>
       </div>
     </div>
   );
